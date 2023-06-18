2a100c9bc8b69383df8edfbcf204dba05c0a000b
ASDRU-B <blancoasdrubal81@gmail.com>
2023-06-18 10:55:20 -0400

    fase3

--- js/main.js
+++ js/main.js
@@ -7,12 +7,36 @@ menuItems.forEach (function (item) {
     })
 })
 
-const toggleCheckbox = document.querySelector('.toggle-checkbox');
-toggleCheckbox.addEventListener('change', function() {
-  const body = document.querySelector('body');
-  if (this.checked) {
-    body.classList.add('dark-mode');
+const toggle = document.getElementById('toggle');
+const header = document.querySelector('header');
+const footer = document.querySelector('footer');
+const buttons = document.querySelectorAll('button[type="button"]');
+
+
+menuItems.forEach(function(item) {
+  item.addEventListener('click', function(e) {
+    const currentItem = document.querySelector('.active');
+    currentItem.classList.remove('active');
+    e.target.classList.add('active');
+  });
+});
+
+toggle.addEventListener('change', function() {
+  if (toggle.checked) {
+    document.body.classList.add('dark-mode');
+    header.classList.add('dark-mode');
+    footer.classList.add('dark-mode');
+    buttons.forEach(function(button) {
+      button.classList.add('dark-mode');
+    });
+    document.getElementById('iconos-contacto').classList.add('dark-mode');
   } else {
-    body.classList.remove('dark-mode');
+    document.body.classList.remove('dark-mode');
+    header.classList.remove('dark-mode');
+    footer.classList.remove('dark-mode');
+    buttons.forEach(function(button) {
+      button.classList.remove('dark-mode');
+    });
+    document.getElementById('iconos-contacto').classList.remove('dark-mode');
   }
 });
